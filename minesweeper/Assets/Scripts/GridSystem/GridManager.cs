006991863f809c0228ba5c25ce5de9293a4df78f
Simla4 <simlabeken4@gmail.com>
2024-09-21 16:31:32 +0300

    feat: added grid system

---
+++ minesweeper/Assets/Scripts/GridSystem/GridManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private int gridWidth = 9;
+    [SerializeField] private int gridHeight = 9;
+    [SerializeField] TileBase tilePrefab;
+
+    private TileBase[,] tiles;
+
+    #endregion
+
+    #region Callbacks
+
+    private void Start()
+    {
+        CreateGrid();
+    }
+
+    #endregion
+
+
+    #region OtherMethods
+
+
+    private void CreateGrid()
+    {
+        tiles = new TileBase[gridWidth, gridHeight];
+
+        for(int x = 0; x < gridWidth; x++)
+        {
+            for(int y = 0; y < gridHeight; y ++)
+            {
+                TileBase newTile = Instantiate(tilePrefab);
+
+                tiles[x, y] = newTile;
+
+                newTile.transform.position = new Vector3(x * 0.4f, y * 0.4f, 0);
+            }
+        }
+    }
+
+    #endregion
+}
---
+++ minesweeper/Assets/Scripts/GridSystem/TileBase.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBase : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private GameObject tileBorder;
+
+    private string tileType;
+
+    #endregion
+}
